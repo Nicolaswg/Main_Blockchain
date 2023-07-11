1d018e2e736ad53c12d68f1858a45782e6a3cc33
Nicolaswg <nigonbol123@gmail.com>
2023-07-11 09:13:51 -0400

    Keep secret keys and password save

--- deploy.js
+++ deploy.js
@@ -8,8 +8,13 @@ async function main() {
   // http://127.0.0.1:7545
 
   const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL);
-  const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
-
+  // const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
+  const encryptedJson = fs.readFileSync("./.encryptedKey.json", "utf8");
+  let wallet = await ethers.Wallet.fromEncryptedJsonSync(
+    encryptedJson,
+    process.env.PRIVATE_KEY_PASSWORD
+  );
+  wallet = wallet.connect(provider);
   const abi = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.abi", "utf8");
   const bytecode = fs.readFileSync(
     "./SimpleStorage_sol_SimpleStorage.bin",
